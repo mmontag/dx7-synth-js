db4417436d6c433e666fd56290f5b5507fc1d035
Matt Montag <montag@spotify.com>
2014-12-07 03:31:41 -0800

    Fix for feedback implementation: use the operator's last output value
    At least that's what Phil Cowans does.
    https://github.com/philcowans/Javascript-DX7

--- operator.js
+++ operator.js
@@ -1,17 +1,19 @@
+// http://www.chipple.net/dx7/fig09-4.gif
 function Operator(frequency, envelope) {
 	this.phase = 0;
+	this.val = 0;
 	this.phaseStep = PERIOD * frequency/SAMPLE_RATE; // radians per sample
 	this.envelope = envelope || new Envelope(0, 3, 0, 0.2);
 }
 
 Operator.prototype.render = function(mod) {
 	mod = mod || 0;
-	var value = Math.sin(this.phase + mod) * this.envelope.render();
+	this.val = Math.sin(this.phase + mod) * this.envelope.render();
 	this.phase += this.phaseStep;
 	if (this.phase >= PERIOD) {
 		this.phase -= PERIOD;
 	}
-	return value;
+	return this.val;
 }
 
 Operator.prototype.noteOff = function() {
