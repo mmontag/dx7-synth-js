b19f08e3a839fb5f28cfc813ad79896a995bb71d
Matt Montag <montag@spotify.com>
2014-12-11 21:42:09 -0800

    Presets WIP

--- app.js
+++ app.js
@@ -2,11 +2,22 @@
 	var app = angular.module('synthApp', []);
 
 	app.controller('ParamsCtrl', function ($scope) {
-		$scope.model = FM_PARAMS;
+		$scope.model = PRESETS[0];
+		$scope.presetz = PRESETS;
+	});
+
+	app.controller('PresetCtrl', function () {
+		this.selectedIndex = 0;
+		this.presets = PRESETS;
+		this.onChange = function() {
+			console.log("changed preset!", this.selectedIndex);
+			window.PRESET_INDEX = this.selectedIndex;
+		};
 	});
 })();
 
 var FM_PARAMS = {
+	name: "Init",
 	algorithm: 1,
 	feedback: 1,
 	operators: [
@@ -66,3 +77,70 @@ var FM_PARAMS = {
 		}
 	]
 };
+var FM_PARAMS2 = {
+	name: "Tubular Bells",
+	algorithm: 8,
+	feedback: .86,
+	operators: [
+		{
+			attack: 0,
+			decay: 4,
+			sustain: 0.2,
+			release: 0.3,
+			freqMult: 1,
+			volume: 1,
+			detune: 5
+		},
+		{
+			attack: 0,
+			decay: 4,
+			sustain: 0.2,
+			release: 0.4,
+			freqMult: 4,
+			volume: 1,
+			detune: 0
+		},
+		{
+			attack: 0,
+			decay: 4,
+			sustain: 0.2,
+			release: 0.3,
+			freqMult: 1,
+			volume: 1,
+			detune: 0
+		},
+		{
+			attack: 0,
+			decay: 4,
+			sustain: 0.2,
+			release: 0.3,
+			freqMult: 1,
+			volume: 1,
+			detune: -5
+		},
+		{
+			attack: 0,
+			decay: 2,
+			sustain: 0.5,
+			release: 0.3,
+			freqMult: 3,
+			volume: 1,
+			detune: 0
+		},
+		{
+			attack: 0,
+			decay: 2,
+			sustain: 0.5,
+			release: 0.3,
+			freqMult: 2,
+			volume: 1,
+			detune: 0
+		}
+	]
+};
+
+var PRESET_INDEX = 0;
+var PRESETS = [
+	FM_PARAMS,
+	FM_PARAMS2
+];
