26aa46e57c53e4c88fe440221dc1e88b6ce5fd12
Matt Montag <montag@spotify.com>
2014-12-14 16:44:24 -0800

    Remove unnecessary quotes in preset

--- app.js
+++ app.js
@@ -97,63 +97,63 @@ var FM_PARAMS = {
 	]
 };
 var FM_PARAMS2 = {
-	"name": "Tubular Bells",
-	"algorithm": "5",
-	"feedback": "0.34",
-	"operators": [
+	name: "Tubular Bells",
+	algorithm: 5,
+	feedback: 0.34,
+	operators: [
 		{
-			"attack": 0,
-			"decay": "3",
-			"sustain": "0.77",
-			"release": "1.6",
-			"freqMult": "2",
-			"volume": "1",
-			"detune": "2"
+			attack: 0,
+			decay: 3,
+			sustain: 0.77,
+			release: 1.6,
+			freqMult: 2,
+			volume: 1,
+			detune: 2
 		},
 		{
-			"attack": "0",
-			"decay": "3",
-			"sustain": "0.85",
-			"release": "1.48",
-			"freqMult": "7",
-			"volume": "0.8",
-			"detune": "3"
+			attack: 0,
+			decay: 3,
+			sustain: 0.85,
+			release: 1.48,
+			freqMult: 7,
+			volume: 0.8,
+			detune: 3
 		},
 		{
-			"attack": "0",
-			"decay": "3",
-			"sustain": "0.73",
-			"release": "1.07",
-			"freqMult": "2",
-			"volume": "1",
-			"detune": "-5"
+			attack: 0,
+			decay: 3,
+			sustain: 0.73,
+			release: 1.07,
+			freqMult: 2,
+			volume: 1,
+			detune: -5
 		},
 		{
-			"attack": 0,
-			"decay": "2.99",
-			"sustain": "0.35",
-			"release": "1.14",
-			"freqMult": "7",
-			"volume": "0.75",
-			"detune": "-2"
+			attack: 0,
+			decay: 2.99,
+			sustain: 0.35,
+			release: 1.14,
+			freqMult: 7,
+			volume: 0.75,
+			detune: -2
 		},
 		{
-			"attack": "0",
-			"decay": "1.33",
-			"sustain": "0",
-			"release": "1.04",
-			"freqMult": "5",
-			"volume": "2.39",
-			"detune": "-5"
+			attack: 0,
+			decay: 1.33,
+			sustain: 0,
+			release: 1.04,
+			freqMult: 5,
+			volume: 2.39,
+			detune: -5
 		},
 		{
-			"attack": "0",
-			"decay": "1.58",
-			"sustain": "0",
-			"release": "1.78",
-			"freqMult": "7",
-			"volume": "0.99",
-			"detune": "-7"
+			attack: 0,
+			decay: 1.58,
+			sustain: 0,
+			release: 1.78,
+			freqMult: 7,
+			volume: 0.99,
+			detune: -7
 		}
 	]
 };
