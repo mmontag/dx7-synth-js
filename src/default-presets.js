97b54834fe611444a1aa79f29f3706ef37f3452e
Matt Montag <montag@spotify.com>
2015-06-23 03:19:08 +0200

    Move default preset definition outside app.js

---
+++ src/default-presets.js
@@ -0,0 +1,106 @@
+var defaultPresets = [
+	{
+		"name": "Init",
+		"algorithm": 18,
+		"feedback": 7,
+		"lfoSpeed": 37,
+		"lfoDelay": 42,
+		"lfoPitchModDepth": 0,
+		"lfoAmpModDepth": 0,
+		"lfoPitchModSens": 4,
+		"lfoWaveform": 4,
+		"lfoSync": 0,
+		"pitchEnvelope": {
+			"rates": [0, 0, 0, 0],
+			"levels": [50, 50, 50, 50]
+		},
+		"operators": [
+			{
+				"idx": 0,
+				"rates": [96, 0, 12, 70],
+				"levels": [99, 95, 95, 0],
+				"detune": 1,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 99,
+				"oscMode": 0,
+				"freqCoarse": 2,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 13.12273
+			},
+			{
+				"idx": 1,
+				"rates": [99, 95, 0, 0],
+				"levels": [99, 96, 89, 0],
+				"detune": -1,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 99,
+				"oscMode": 0,
+				"freqCoarse": 0,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 13.12273
+			},
+			{
+				"idx": 2,
+				"rates": [99, 87, 0, 0],
+				"levels": [93, 90, 0, 0],
+				"detune": 0,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 82,
+				"oscMode": 0,
+				"freqCoarse": 1,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 3.008399
+			},
+			{
+				"idx": 3,
+				"rates": [99, 92, 28, 60],
+				"levels": [99, 90, 0, 0],
+				"detune": 2,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 71,
+				"oscMode": 0,
+				"freqCoarse": 2,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 1.159897
+			},
+			{
+				"idx": 4,
+				"rates": [99, 99, 97, 0],
+				"levels": [99, 65, 60, 0],
+				"detune": -2,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 43,
+				"oscMode": 0,
+				"freqCoarse": 3,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 0.102521
+			},
+			{
+				"idx": 5,
+				"rates": [99, 70, 60, 0],
+				"levels": [99, 99, 97, 0],
+				"detune": 0,
+				"velocitySens": 0,
+				"lfoAmpModSens": 0,
+				"volume": 47,
+				"oscMode": 0,
+				"freqCoarse": 17,
+				"freqFine": 0,
+				"pan": 0,
+				"outputLevel": 0.144987
+			}
+		]
+	}
+];
+
+module.exports = defaultPresets;
